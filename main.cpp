2b22824172166a92a32cd51fa25e0ba7b4eaabe6
vassko02 <vass.kornel@gmail.com>
2024-09-05 13:03:44 +0200

    hibák kommentelve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //elírás
+    std::cout << '1-100 ertekek duplazasa' //' helyett " kell
+    for (int i = 0;) // hibás a ciklus fejléce for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //i < N_ELEMENTS kellene
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // nem írja ki az értéket csak azt hogy "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //; helyett , kell
     {
-        atlag += b[i]
+        atlag += b[i] //sor végi ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
