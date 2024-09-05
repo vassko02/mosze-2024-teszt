664670f06c29ca8d9db9c79fa70dcfd47cba1545
vassko02 <vass.kornel@gmail.com>
2024-09-05 13:05:07 +0200

    hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,34 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //elírás
-    std::cout << '1-100 ertekek duplazasa' //' helyett " kell
-    for (int i = 0;) // hibás a ciklus fejléce for (int i = 0; i < N_ELEMENTS; i++)
+    int *b = new int[N_ELEMENTS]; // 'NELEMENTS' helyett 'N_ELEMENTS'
+    
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // ' helyett " kell
+    
+    for (int i = 0; i < N_ELEMENTS; i++) // hibás a ciklus fejléce javítva
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //i < N_ELEMENTS kellene
+    
+    for (int i = 0; i < N_ELEMENTS; i++) // i < N_ELEMENTS helyesen
     {
-        std::cout << "Ertek:" // nem írja ki az értéket csak azt hogy "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl; // Kiírja az értékeket is
     }    
+    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //; helyett , kell
+    
+    int atlag = 0; // Az átlagot nulláról kell indítani
+    
+    for (int i = 0; i < N_ELEMENTS; i++) // ; helyett , javítva
     {
-        atlag += b[i] //sor végi ; hiányzik
+        atlag += b[i]; // Sor végi ; javítva
     }
+    
     atlag /= N_ELEMENTS;
+    
     std::cout << "Atlag: " << atlag << std::endl;
+    
+    delete[] b; // Felszabadítjuk a dinamikusan lefoglalt memóriát
+    
     return 0;
 }
